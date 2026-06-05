2303246d597fba960258dbc1b714575408f061bf
fachrydwz <fachrydwizakaria61@gmail.com>
2026-06-05 19:15:29 +0700

    fix: env variable dan cors untuk deploy

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,10 @@ const modulDetailRoutes = require('./routes/modulDetail');
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
